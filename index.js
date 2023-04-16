82ddb4e5e440842d82315a0c8848da70fbb96757
Watheq Attrah <novaatrah@gmail.com>
2023-04-16 16:01:24 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+// document.addEventListener("DOMContentLoaded",paraReplace);
+
+
+// function paraReplace(){
+//     document.getElementById("#text");
+//     let p=document.getElementById('text');
+//     p.textContent= "This is really cool!"
+//     console.log(p.textContent);
+// }
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector('p');
+    let p=document.getElementById("text");
+    p.textContent="This is really cool!";
+})
